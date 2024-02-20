1f058aa7b9a340537df857f1ecd6c2eba1cdf384
unknown <ramir.web.dev@gmail.com>
2024-02-21 02:54:31 +0400

    Chat simple ui with socket

---
+++ src/types/common.ts
@@ -0,0 +1,11 @@
+export type MessageType = {
+  senderId: string;
+  sender: string;
+  message: string;
+  timestamp: string
+}
+
+export type SignedUserType = {
+  id: string;
+  username: string;
+}
